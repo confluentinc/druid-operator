abe2108ca50b993bd917cb5659e3acce9b6130a5
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2022-05-16 12:32:31 +0530

    make nodeselector consistent (#284)
    * make nodeselector consitent
    
    * update handler

--- controllers/druid/handler.go
+++ controllers/druid/handler.go
@@ -1158,7 +1158,7 @@ func makePodTemplate(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid, ls map
 // makePodSpec shall create podSpec common to both deployment and statefulset.
 func makePodSpec(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid, nodeSpecUniqueStr, configMapSHA string) v1.PodSpec {
 	spec := v1.PodSpec{
-		NodeSelector:     m.Spec.NodeSelector,
+		NodeSelector:              firstNonNilValue(m.Spec.NodeSelector, nodeSpec.NodeSelector).(map[string]string),
 		Tolerations:      getTolerations(nodeSpec, m),
 		Affinity:         getAffinity(nodeSpec, m),
 		ImagePullSecrets: firstNonNilValue(nodeSpec.ImagePullSecrets, m.Spec.ImagePullSecrets).([]v1.LocalObjectReference),
