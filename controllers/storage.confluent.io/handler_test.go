1607d8a681d9794385e5f9512b522b7c3b67859d
Sadananda Aithal <111732128+saithal-confluent@users.noreply.github.com>
2023-07-04 14:35:20 +0530

    Add local storage controller and CRD (#24)
    * Initial commit
    
    * Minor fixes and corrections
    
    * Adds images as mandatory fields
    
    * Adds configmap for local-volume-provisioner
    
    * Adds serviceAccountName and storageClass
    
    * Use controller-gen v0.9.2
    
    * Revert controller-gen changes
    
    * Test configs changes
    
    * Adds labels to created pv
    
    * webhook commented for now
    
    * Adds cleanup job
    
    * Delete PVs from the operator itself
    
    * Listing PVs and adding ownerref
    
    * List and delete PV fixes
    
    * Unit tests
    
    * Fix unit tests
    
    * Fixes configMap name of localVolumeProvisioner
    
    * Removes name from localstorage cr
    
    * Fixes removal of name from CR
    
    * Updated replicas in node grabber deployment
    
    * Renames namespace in deploy configs from sada-test to default
    
    * Rebuild
    
    * Removes webhook of localstorage
    
    * Address comments
    
    * Addressing review comments
    
    * Addresses review comments
    
    * Fix unit-tests
    
    * Removes creation of storageClass from localStorage operator
    
    * Fixes druid role to add permissions for LocalStorage
    
    * Change deploy operator image
    
    * Address missed review comments

---
+++ controllers/storage.confluent.io/handler_test.go
@@ -0,0 +1,151 @@
+package controllers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"reflect"
+	"testing"
+
+	storageconfluentiov1 "github.com/datainfrahq/druid-operator/apis/storage.confluent.io/v1"
+	"github.com/ghodss/yaml"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+)
+
+func setEmptyResource(obj interface{}) interface{} {
+	switch obj.(type) {
+	case *appsv1.Deployment:
+		deployment := obj.(*appsv1.Deployment)
+		for i, _ := range deployment.Spec.Template.Spec.Containers {
+			deployment.Spec.Template.Spec.Containers[i].Resources = corev1.ResourceRequirements{}
+		}
+		return deployment
+	case *appsv1.StatefulSet:
+		statefulset := obj.(*appsv1.StatefulSet)
+		for i, _ := range statefulset.Spec.Template.Spec.Containers {
+			statefulset.Spec.Template.Spec.Containers[i].Resources = corev1.ResourceRequirements{}
+		}
+		return statefulset
+	}
+	return nil
+}
+
+func TestMakeDaemonsetForENP(t *testing.T) {
+	clusterSpec := readSampleLSSpec(t)
+	eksNvmeProvisionerPodSpec, eksNvmeProvisionerLabels := makeEKSNVMEProvisioner(clusterSpec)
+
+	actual, _ := makeDaemonSet(clusterSpec, eksNvmeProvisionerPodSpec, eksNvmeProvisionerLabels)
+	addHashToObject(actual)
+
+	actual.Spec.Template.Spec.Containers[0].Resources = corev1.ResourceRequirements{}
+
+	expected := new(appsv1.DaemonSet)
+	readAndUnmarshallResource("testdata/enp-daemonset.yaml", &expected, t)
+
+	assertEquals(expected, actual, t)
+
+}
+
+func TestMakeDaemonsetForLVP(t *testing.T) {
+	clusterSpec := readSampleLSSpec(t)
+
+	localVolumeProvisionerPodSpec, localVolumeProvisionerLabels := makeLocalVolumeProvisioner(clusterSpec)
+
+	actual, _ := makeDaemonSet(clusterSpec, localVolumeProvisionerPodSpec, localVolumeProvisionerLabels)
+	addHashToObject(actual)
+	updatedActual := setEmptyResource(&actual.Spec.Template.Spec)
+
+	expected := new(appsv1.DaemonSet)
+	readAndUnmarshallResource("testdata/lvp-daemonset.yaml", &expected, t)
+	updatedExpected := setEmptyResource(&expected.Spec.Template.Spec)
+
+	assertEquals(updatedExpected, updatedActual, t)
+}
+
+func TestMakeDeploymentForNodeGrabber(t *testing.T) {
+	clusterSpec := readSampleLSSpec(t)
+
+	nodeGrabberPodSpec, nodeGrabberLabels := makeNodeGrabber(clusterSpec)
+
+	actual, _ := makeDeployment(clusterSpec, nodeGrabberPodSpec, nodeGrabberLabels)
+	addHashToObject(actual)
+	updatedActual := setEmptyResource(&actual.Spec.Template.Spec)
+
+	expected := new(appsv1.Deployment)
+	readAndUnmarshallResource("testdata/nodegrabber-deployment.yaml", &expected, t)
+	updatedExpected := setEmptyResource(&expected.Spec.Template.Spec)
+
+	assertEquals(updatedExpected, updatedActual, t)
+}
+
+func TestMakeConfigMap(t *testing.T) {
+	clusterSpec := readSampleLSSpec(t)
+
+	configMapData, configMapLabels := makeLocalVolumeProvisionerConfigMap(clusterSpec)
+	actual, _ := makeConfigMap(clusterSpec, configMapLabels, configMapData)
+	addHashToObject(actual)
+
+	expected := new(corev1.ConfigMap)
+	readAndUnmarshallResource("testdata/lvp-config-map.yaml", &expected, t)
+	assertEquals(expected, actual, t)
+}
+
+func readSampleLSSpec(t *testing.T) *storageconfluentiov1.LocalStorage {
+	return readLSSpecFromFile(t, "testdata/localstorage-smoke-test-cluster.yaml")
+}
+
+func readNamespaceSpecFromFile(t *testing.T, filePath string) *v1.Namespace {
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		t.Errorf("Failed to read spec")
+	}
+	spec := new(v1.Namespace)
+	err = yaml.Unmarshal(bytes, &spec)
+	if err != nil {
+		t.Errorf("Failed to unmarshall spec: %v", err)
+	}
+
+	return spec
+}
+
+func readLSSpecFromFile(t *testing.T, filePath string) *storageconfluentiov1.LocalStorage {
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		t.Errorf("Failed to read druid cluster spec")
+	}
+
+	clusterSpec := new(storageconfluentiov1.LocalStorage)
+	err = yaml.Unmarshal(bytes, &clusterSpec)
+	if err != nil {
+		t.Errorf("Failed to unmarshall druid cluster spec: %v", err)
+	}
+
+	return clusterSpec
+}
+
+func readAndUnmarshallResource(file string, res interface{}, t *testing.T) {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		t.Errorf("Failed to read file[%s]", file)
+	}
+
+	err = yaml.Unmarshal(bytes, res)
+	if err != nil {
+		t.Errorf("Failed to unmarshall resource from file[%s]", file)
+	}
+}
+
+func assertEquals(expected, actual interface{}, t *testing.T) {
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error("Match failed!.")
+	}
+}
+
+func dumpResource(res interface{}, t *testing.T) {
+	bytes, err := yaml.Marshal(res)
+	if err != nil {
+		t.Errorf("failed to marshall: %v", err)
+	}
+	fmt.Println(string(bytes))
+}
